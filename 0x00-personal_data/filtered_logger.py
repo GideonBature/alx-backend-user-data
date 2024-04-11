b4e1174433e5f1b860d904267335f3548eea56c0
GideonBature <infoaboutgideon@gmail.com>
2024-04-11 09:42:18 +0100

    fix pycodestyle

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,7 +8,6 @@ from typing import List
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    """Obfuscate log messages"""
     for field in fields:
         message = re.sub(f"{field}=.*?{separator}",
                          f"{field}={redaction}{separator}", message)
