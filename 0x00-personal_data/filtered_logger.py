fa9402b0b780b02854636cb51984b3932e67131b
GideonBature <infoaboutgideon@gmail.com>
2024-04-11 04:08:32 +0100

    fix code to meet requirements

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,10 +4,8 @@
 import re
 
 
-def filter_datum(fields: list, redaction: str,
-                 message: str, separator: str) -> str:
+def filter_datum(fields: list, redaction: str, message: str, separator: str) -> str:
     """Obfuscate log messages"""
     for field in fields:
-        message = re.sub(f"{field}=.*?{separator}",
-                         f"{field}={redaction}{separator}", message)
+        message = re.sub(f"{field}=.*?{separator}", f"{field}={redaction}{separator}", message)
     return message
