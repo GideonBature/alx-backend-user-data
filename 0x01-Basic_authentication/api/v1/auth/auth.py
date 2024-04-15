976cce041f89ddc3fc5a4d03a4687633a4c3bed4
GideonBature <infoaboutgideon@gmail.com>
2024-04-15 22:45:25 +0100

    Update the method def require_auth(self, path: str, excluded_paths: List[str]) -> bool: in Auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,6 +12,14 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ require_auth
         """
+        if path is None:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path not in excluded_paths:
+            return True
+        if excluded_paths is None or excluded_paths == []:
+            return True
         return False
 
     def authorization_header(self, request=None) -> str:
