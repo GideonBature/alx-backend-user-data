c80bc734ea96a249f61a0049cbb50f496f6aeaae
GideonBature <infoaboutgideon@gmail.com>
2024-04-15 22:20:51 +0100

    a class to manage the API authentication.

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Auth class
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ Auth class
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ require_auth
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ authorization_header
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current_user
+        """
+        return None
