6aa5156cc4ad54f3161b85efb5c7848c381636d7
GideonBature <infoaboutgideon@gmail.com>
2024-04-16 15:23:38 +0100

    fix code, import base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ Basic Auth Class
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -19,3 +20,19 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """ decode_base64_authorization_header
+        """
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) != str:
+            return None
+        try:
+            d_bytes = base64.b64decode(base64_authorization_header)
+            decoded_string = d_bytes.decode('utf-8')
+            return decoded_string
+        except Exception:
+            return None
