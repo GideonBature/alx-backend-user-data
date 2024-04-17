a05949dd660a2e68d67a75eb69b9c3e6b55ddd75
GideonBature <infoaboutgideon@gmail.com>
2024-04-17 01:46:24 +0100

    Improve def require_auth(self, path, excluded_paths) by allowing * at the end of excluded paths.

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 """
 from flask import request
 from typing import List, TypeVar
+from fnmatch
 
 
 class Auth:
@@ -15,14 +16,21 @@ class Auth:
         if excluded_paths is None or excluded_paths == []:
             return True
         for excluded_path in excluded_paths:
-            if excluded_path[-1] != '/':
+            if excluded_path[-1] != '/' and excluded_path[-1] != '*':
                 excluded_path += '/'
+
         if path is None:
             return True
+
+        for p in excluded_paths:
+            if fnmatch.fnmatch(path, p):
+                return False
+
         if path[-1] != '/':
             path += '/'
         if path not in excluded_paths:
             return True
+
         return False
 
     def authorization_header(self, request=None) -> str:
