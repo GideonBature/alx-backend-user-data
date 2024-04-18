5978bee2c8719d59ee0792c4d63e5e9f2b1cc260
GideonBature <infoaboutgideon@gmail.com>
2024-04-18 12:50:16 +0100

    a new endpoint: GET /users/me to retrieve the authenticated User object.

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -39,6 +39,7 @@ def before_request() -> str:
     if auth.require_auth(request.path, excluded_paths):
         if auth.authorization_header(request) is None:
             abort(401)
+        request.current_user = auth.current_user(request)
         if auth.current_user(request) is None:
             abort(403)
 
