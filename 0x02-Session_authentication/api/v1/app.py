17c3472eec58f9b247ac9306534ea3d79d7726f7
GideonBature <infoaboutgideon@gmail.com>
2024-04-18 14:58:17 +0100

    update @app.before_request method in api/v1/app.py adding a URL path to the list of excluded paths of the method require_auth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -37,10 +37,12 @@ def before_request() -> str:
     excluded_paths = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
     ]
     if auth.require_auth(request.path, excluded_paths):
-        if auth.authorization_header(request) is None:
+        if auth.authorization_header(request) is None and \
+                auth.session_cookie(request) is None:
             abort(401)
         request.current_user = auth.current_user(request)
         if auth.current_user(request) is None:
