0e0451f52c6a524d7fead509748c5bd38f3abf70
GideonBature <infoaboutgideon@gmail.com>
2024-04-18 22:35:53 +0100

    fix code

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """ Session DB Auth
 """
-
-
+from datetime import datetime, timedelta
 from api.v1.auth.session_exp_auth import SessionExpAuth
 from models.user_session import UserSession
 
@@ -10,22 +9,26 @@ from models.user_session import UserSession
 class SessionDBAuth(SessionExpAuth):
     """ Session DB Auth
     """
-
     def create_session(self, user_id: str = None) -> str:
         """ create_session
         """
         session_id = super().create_session(user_id)
         if session_id:
-            session = UserSession(user_id=user_id, session_id=session_id)
-            session.save()
+            n_session = UserSession(user_id=user_id, session_id=session_id)
+            n_session.save()
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """ user_id_for_session_id
         """
-        session = UserSession.search({'session_id': session_id})
-        if session:
-            return session[0].user_id
+        user_sessions = UserSession.search({'session_id': session_id})
+        if user_sessions:
+            user_session = user_sessions[0]
+            if self.session_duration > 0:
+                time_elapsed = datetime.now() - user_session.created_at
+                if time_elapsed > timedelta(seconds=self.session_duration):
+                    return None
+            return user_session.user_id
         return None
 
     def destroy_session(self, request=None) -> bool:
