43869c9376ae90dee99ee4ae5c7b9f3752045c35
GideonBature <infoaboutgideon@gmail.com>
2024-04-25 12:36:22 +0100

    fix code

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -64,7 +64,8 @@ class DB:
         """
         user = self.find_user_by(id=user_id)
         for key, value in kwargs.items():
-            if key not in User.__table__.columns.keys():
+            if hasattr(user, key):
+                setattr(user, key, value)
+            else:
                 raise ValueError
-            setattr(user, key, value)
         self._session.commit()
